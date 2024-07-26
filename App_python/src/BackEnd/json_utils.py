cb922dc8b7664b81e0baf5b1b7c7079a75f52907
RYUseless <janfilipzacek@seznam.cz>
2024-07-26 12:35:55 +0200

    Logic update v2 -- Json logic, some simple finding etc. , Shell script not fully tested once again, will do soon.

--- App_python/src/BackEnd/json_utils.py
+++ App_python/src/BackEnd/json_utils.py
@@ -1,18 +1,73 @@
-# import json
+import json
 import os
+import sys
+
+__json_config_name = "config.json"
 
 
-def _go_to_assets():
-    working_dir = os.getcwd()
-    wrk_one_up = os.path.abspath(os.path.join(working_dir, "../"))
-    location_assets = os.path.join(wrk_one_up, "assets")
-    loc_ass_boolean = os.path.exists(location_assets)
+class actions:
+    def __init__(self):
+        self.json_config_name = "config.json"
+        self.assets_location = None
 
-    if loc_ass_boolean:
-        return True, location_assets
-    else:
-        return False, None
+    @staticmethod
+    def find_json_folder():
+        working_dir = os.getcwd()
+        current_dir_name = os.path.basename(os.path.abspath(working_dir))
 
+        # finding directory one bellow assets folder.
+        print(f"OS namae {sys.platform}")
+        while current_dir_name != "App_python" and working_dir != "/":
+            working_dir = os.path.abspath(os.path.join(working_dir, "../"))
+            current_dir_name = os.path.basename(os.path.abspath(working_dir))
 
-def find_json_folder():
-    print("todo")
+        location_assets = os.path.join(working_dir, "assets")
+        loc_ass_boolean = os.path.exists(location_assets)
+
+        if loc_ass_boolean is False:
+            print("There is no valid path, for now exiting")
+            # TODO: try to create assets folder once again, if path itself is corrupted, idk, delete whole OS.
+            exit(1)
+        else:
+            return location_assets
+
+    def find_json_config(self):
+        if self.assets_location is None:
+            self.assets_location = self.find_json_folder()
+        else:
+            pass
+
+        full_path = os.path.join(self.assets_location, self.json_config_name)
+        path_verify = os.path.exists(full_path)
+        if path_verify is False:
+            print("No config file was found, generating default config now...")
+            self.write_default_json(self.assets_location)
+            return full_path
+        else:
+            return full_path
+
+    def read_config(self, key, value):
+        path = self.find_json_config()  # maybe optimize this?  potential power inefficiency
+        with open(path, "r") as file:
+            jsonData = json.load(file)
+        return jsonData[key][value]
+
+    def write_default_json(self, path):
+        # percentage out of whole screen for app dimensions:
+        # first is percentage for X_axis, then for Y_axis
+        window_percentage = [50, 50]
+        dictionary = {
+            'database_config': {
+                "Port": 6969,
+                "url": "tvojeMamka.com"
+            },
+            'FrontEnd_config': {
+                "App_window": window_percentage.copy(),
+                "App_def_screenId": 0,
+
+            },
+
+        }
+        json_object = json.dumps(self, dictionary, indent=4)
+        with open(os.path.join(path, self.json_config_name), "w+", encoding='utf-8') as outfile:
+            outfile.write(json_object)
--- App_python/src/FrontEnd/main_gui.py
+++ App_python/src/FrontEnd/main_gui.py
@@ -2,14 +2,17 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 # from PyQt5.QtCore import Qt
 from src.BackEnd import screen_utils
+import src.BackEnd.json_utils as Json_utils
 
 
 class Window:
     def __init__(self):
         _get_info_instance = screen_utils.get_info()
-        self.screen_x, self.screen_y, self.window_width, self.window_height =\
-            _get_info_instance.window_geometry(80, 60)
-        self.monitor_id = 0
+        Json_utils_instance = Json_utils.actions()
+        _lengh_percentage, _high_percentage = Json_utils_instance.read_config("FrontEnd_config", "App_window")
+        self.monitor_id = Json_utils_instance.read_config("FrontEnd_config", "App_def_screenId")
+        self.screen_x, self.screen_y, self.window_width, self.window_height = \
+            _get_info_instance.window_geometry(_lengh_percentage, _high_percentage)
 
     def root(self):
         app = QApplication(sys.argv)
@@ -23,7 +26,7 @@ class Window:
             screen_geometry = screens[self.monitor_id].geometry()
             screen_center = screen_geometry.center()
 
-            # Nastavení geometrie okna
+            # window settings
             window_geometry = mainwindow.frameGeometry()
             window_geometry.moveCenter(screen_center)
             mainwindow.move(window_geometry.topLeft())
